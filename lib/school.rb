d93f27353472a7fbcfafe01bb8505edf15778986
Alison Williams <apw5011@gmail.com>
2020-11-20 00:14:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,7 @@
 class School
   
-  def initialize(name, roster) 
+  def initialize(name) 
     @name = name
-    @roster = {}
   end 
-  
-  def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name 
-  end
   
 end 
